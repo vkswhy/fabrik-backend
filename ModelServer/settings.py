bb97552cdb627b3c6fc4b0637ac367da10e9542d
vkswhy <vkswhy@gmail.com>
2022-06-06 16:22:20 +0530

    updated to testmanian style

--- ModelServer/settings.py
+++ ModelServer/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import django_on_heroku
 from pathlib import Path
 import os
 django_on_heroku.settings(locals())
@@ -123,8 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
+
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),"media")
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 print("settings media root", MEDIA_ROOT,BASE_DIR)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
